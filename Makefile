b5181d951103066ac17fa5b8752044a74365a00e
Don Isaac <donald.isaac@gmail.com>
2021-09-23 19:19:19 -0400

    Added test coverage and doc comments

--- Makefile
+++ Makefile
@@ -1,14 +1,42 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Wconversion -pedantic -Iincludes -std=c99 # LDLIBS=-lstdc++
 VPATH = src src/map test
+LDLIBS = -lgcov --coverage
+
+TARGETS = bst
+FOLDERS = ./ src/ src/map/ test/
 
 ifdef DEBUG
-	CFLAGS += -DDEBUG -ggdb
+	CFLAGS += -DDEBUG -ggdb -fprofile-arcs -ftest-coverage
 endif
 
 .PHONY: all
-all: bst
+all: $(TARGETS)
 
-bst: test/bst.o bintree.o
+# ================================== TESTING ===================================
+
+.PHONY: *.report
+
+# bst.report: test/bst.o bintree.o
+bst.report: bst
+	./bst
+	gcov -a -c -f test/bst.c src/map/bintree.o
+# %.report: %
+# 	gcov test/$<.c
+bst: test/bst.o src/map/bintree.o
 
 # %.tst: test/%.o
+
+# ==================================== UTIL ====================================
+
+.PHONY: install clean
+
+install:
+	apt-get install gcov doxygen -y
+
+clean:
+	$(RM) -rf \
+		$(addsuffix *.o, $(FOLDERS)) \
+		$(addsuffix *.gcov, $(FOLDERS)) \
+		$(addsuffix *.gcno, $(FOLDERS)) \
+		$(TARGETS)
--- src/map/bintree.h
+++ src/map/bintree.h
@@ -2,14 +2,47 @@
 #define __BINTREE_H__
 
 #include <stdlib.h>
+
 #include "map.h"
 
 typedef struct bt_bintree BinTree;
 
+/**
+ * @brief Constructs a new BinTree.
+ *
+ * @param tree A pointer to the tree to construct.
+ *
+ * @return int 1 on success, 0 on failure.
+ */
 int bt_init(BinTree **tree);
-int bt_free(BinTree **free);
 
+/**
+ * @brief Destroys an existing Bintree and frees all resources associated with it.
+ *
+ * After destruction, the BinTree will be set to `NULL`.
+ *
+ * @param tree A pointer to the tree to destroy.
+ *
+ * @return int 1 on success, 0 on failure.
+ */
+int bt_free(BinTree **tree);
+
+/**
+ * @brief Calculates the height of a BinTree.
+ *
+ * @param tree The target tree.
+ * @return int The height of the tree, or 0 on failure. Check `errno` to
+ * distinguish between a tree with a height of `0` and failures.
+ */
 int bt_height(BinTree *tree);
+
+/**
+ * @brief Gets the number of entries in a BinTree.
+ *
+ * @param tree The target tree.
+ * @return int The number of entries in the tree. On failure, 0 is returned.
+ * To distinguish between a tree with no entries and a failure, check `errno`.
+ */
 int bt_size(BinTree *tree);
 
 /**
@@ -26,7 +59,27 @@ int bt_size(BinTree *tree);
  * is replaced, 2 is returned.
  */
 int bt_add(BinTree *tree, char *key, void *data, size_t size);
-void* bt_get(BinTree *tree, char *key);
+
+/**
+ * @brief Searches the BinTree for an entry.
+ *
+ * @param tree The tree to search.
+ * @param key The key the entry is stored under.
+ *
+ * @return void* A pointer to the data stored in the entry. If no entry exists
+ * for the given key, `NULL` is returned. On error, `NULL` is returned and
+ * `errno` is set.
+ */
+void *bt_get(BinTree *tree, char *key);
+
+/**
+ * @brief Checks if an entry for a key exists.
+ *
+ * @param tree The tree to search.
+ * @param key  The entry key to check.
+ *
+ * @return int 1 if an entry exists for `key`, 0 if one does not.
+ */
 int bt_has(BinTree *tree, char *key);
 int bt_remove(BinTree *tree, char *key);
 #endif
